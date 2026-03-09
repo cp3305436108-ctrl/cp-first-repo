9a04f71b19adc1b65474dadf3aa9de6ac360b0be
hedong007 <cp3305436108@gmail.com>
2026-03-09 18:56:38 +0800

    Add files via upload

---
+++ demo01.py
@@ -0,0 +1,44 @@
+"""
+============================
+Project: py48
+Author:柠檬班-海励
+Time:2022/1/25 21:41
+E-mail:2227092769@qq.com
+Company:湖南零檬信息技术有限公司
+Site: http://www.lemonban.com
+Forum: http://testingpai.com 
+============================
+"""
+"""
+字体设置
+File ---  Settings --- Editor -- Font ---size
+
+主题设置(ide的界面颜色)
+File ---  Settings --- Editor --- Color Scheme
+
+配置python解释器
+File ---  Settings --- Project -- Python Interpreter 
+
+自动生成注释文字
+File ---  Settings --- Editor --- file and code templates
+============================
+Project: ${PROJECT_NAME}
+Author:柠檬班-海励
+Time:${DATE} ${TIME}
+E-mail:2227092769@qq.com
+Company:湖南零檬信息技术有限公司
+Site: http://www.lemonban.com
+Forum: http://testingpai.com 
+============================
+
+python文件执行
+1、右键run(快捷键ctrl+sheft+F10)
+2、导航栏绿色三角箭头
+3、控制台绿色三角箭头
+
+"""
+
+print("hello python")
+
+
+
